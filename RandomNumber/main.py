bc547b38e363ad6d8dfaea0f9699e8658647d92a
Nazar Kovtun <68727071+NazarKovtun@users.noreply.github.com>
2024-08-23 21:35:01 +0300

    Update main.py
    Edited for easier user experience

--- RandomNumber/main.py
+++ RandomNumber/main.py
@@ -1,30 +1,22 @@
-from Rnd09AZ import Rnd09AZ
-from RndNAZ import RndNAZ
-from Ns import Ns
-from NsFactory import NsFactory
-from Rnd import Rnd
+"""
+You can use:
+- RndReal;
+- RndPython;
+- RndPseudo;
+- RndInt;
+- RndNAZ;
+- Rnd09AZ.
+"""
 from RndPseudo import RndPseudo
-from RndReal import RndReal
-from RndPython import RndPython
-from RndInt import RndInt
+from NsFactory import NsFactory
+from Ns import Ns
 
 
 def main() -> None:
-    r2: Rnd = RndPython(1)
-    # r: Rnd = RndPseudo()
-
-    rnd = Rnd09AZ(1)
-    ns_fac = NsFactory(rnd)
-    ns = ns_fac.ns()
+    rnd = RndPseudo()
+    ns_factory = NsFactory(rnd)
+    ns = ns_factory.ns()
     ns.print()
-
-    ns2_factory = NsFactory(r2)
-    ns2 = ns2_factory.ns()
-    ns2.print()
-
-    # ns_factory = NsFactory(r)
-    # ns = ns_factory.ns()
-    # ns.print()
 
 
 if __name__ == "__main__":
